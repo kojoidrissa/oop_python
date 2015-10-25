61432b8584d90c789c65488911994384ea8866ca
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-25 09:27:49 -0500

    added docstrings to class MaxSizeList

--- Chap3/assignments.py
+++ Chap3/assignments.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python2.7
 
 class MaxSizeList(object):
+    '''
+    MaxSizeList(int) --> object containing a list with a maximum length
+    of int
+    '''
     def __init__(self, size):
         self.maxlen = size
         self.struct = []
 
     def push(self, item):
+        ''' Appends an item to the list in the object'''
         self.struct.append(item)
         if len(self.struct) > self.maxlen:
             self.struct.pop(0)
 
     def get_list(self):
+        '''returns the list'''
         return self.struct
