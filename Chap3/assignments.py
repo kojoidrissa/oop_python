7e6ebfafa7cdeb9a852855b26e2ce709f143822a
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-25 09:39:27 -0500

    renamed object attributes for clarity

--- Chap3/assignments.py
+++ Chap3/assignments.py
@@ -6,15 +6,15 @@ class MaxSizeList(object):
     of int
     '''
     def __init__(self, size):
-        self.maxlen = size
-        self.struct = []
+        self.max_size = size
+        self.inner_struct = []
 
     def push(self, item):
         ''' Appends an item to the list in the object'''
-        self.struct.append(item)
-        if len(self.struct) > self.maxlen:
-            self.struct.pop(0)
+        self.inner_struct.append(item)
+        if len(self.inner_struct) > self.max_size:
+            self.inner_struct.pop(0)
 
     def get_list(self):
         '''returns the list'''
-        return self.struct
+        return self.inner_struct
